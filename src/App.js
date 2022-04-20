826c4f92435720bb0052f78b0d3ab3d2a86577d8
Karthikeyan Jaganathan <karthikeyanjaganathan@Karthikeyans-MacBook-Air.local>
2022-04-20 18:32:49 +0530

    accessed data from firebase

--- src/App.js
+++ src/App.js
@@ -2,32 +2,29 @@ import React from "react";
 import Cart from "./Cart";
 import Navbar from "./Navbar";
 
+//importing in all the required functions only ->thats how firebase V9 works
+import {initializeApp} from 'firebase/app'
+import {getFirestore,collection,getDocs} from 'firebase/firestore'
+
+//this info which is unique to our firebase project
+const firebaseConfig = {
+  apiKey: "AIzaSyDeJ4lbSFGKEHRgMsvyTjn3RGDnsS6JD70",
+  authDomain: "cart-app-fca2a.firebaseapp.com",
+  projectId: "cart-app-fca2a",
+  storageBucket: "cart-app-fca2a.appspot.com",
+  messagingSenderId: "234435207195",
+  appId: "1:234435207195:web:8dce9e18999b3b099218be"
+};
+
+
+
+
 class App extends React.Component{
   constructor(){
     super();
     this.state={
       products:[
-        {
-          title:'MOBILE PHONE',
-          price:9,
-          qty:1,
-          img:'https://images.unsplash.com/photo-1580910051074-3eb694886505?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          id:1
-        },
-        {
-          title:'WATCH',
-          price:99,
-          qty:2,
-          img:'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8d2F0Y2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          id:2
-        },
-        {
-          title:'LAPTOP',
-          price:999,
-          qty:3,
-          img:'https://images.unsplash.com/photo-1597672996375-4d21cad0cbb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGFwdG9wJTIwbW9ja3VwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
-          id:3
-        }
+        //we removed everything coz now we linked firebase to the app na
       ]
     }
   }
@@ -76,6 +73,23 @@ class App extends React.Component{
     })
     return s
   }
+
+  //nicely use this boi to take in data from firebase
+  componentDidMount(){
+    const app=initializeApp(firebaseConfig)   //starting the firebase stuff
+    const db=getFirestore(app)                //accessing the firestore database
+    const colref=collection(db,'products')    //touching the collection we need
+    getDocs(colref).then((snapshot)=>{        // touching the docs we need one at a time, and then 
+      const products=snapshot.docs.map((product)=>{  //putting them inside an array, to set that as 
+      return (product.data())                        //our state ->yessir
+      })
+      this.setState({
+        products
+      })
+    })
+    
+  }
+
 
   render(){
     const {products}=this.state
--- src/index.js
+++ src/index.js
@@ -3,22 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-//and this is the code from the codebase website to get our firebase integrated
-
-import { initializeApp } from "firebase/app";
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDeJ4lbSFGKEHRgMsvyTjn3RGDnsS6JD70",
-  authDomain: "cart-app-fca2a.firebaseapp.com",
-  projectId: "cart-app-fca2a",
-  storageBucket: "cart-app-fca2a.appspot.com",
-  messagingSenderId: "234435207195",
-  appId: "1:234435207195:web:8dce9e18999b3b099218be"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
